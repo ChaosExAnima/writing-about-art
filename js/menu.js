69954c9a94857a44c37b4c2e1a2f844744023ddb
Echo <ChaosExAnima@users.noreply.github.com>
2023-02-07 17:50:28 -0500

    Update GA and formats everything (#1)
    * Updates GA tag + formatter
    
    * Add prettier
    
    * Formatting

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,22 @@
 function init() {
 	var oId;
-	for (i=1;i<5;i++) {
-		oId = document.getElementById("m0"+i);
-		oId.style.position = "absolute";
-		oId.style.left = "120px";
-		oId.style.marginTop = "-3.3em";
-		if (i==3) {
-			oId.style.marginTop = "-2.3em";
+	for (i = 1; i < 5; i++) {
+		oId = document.getElementById('m0' + i);
+		oId.style.position = 'absolute';
+		oId.style.left = '120px';
+		oId.style.marginTop = '-3.3em';
+		if (i == 3) {
+			oId.style.marginTop = '-2.3em';
 		}
 	}
 }
-		
 
 function show(id) {
 	var oId = document.getElementById(id);
-	oId.style.display = "block";
+	oId.style.display = 'block';
 }
 
 function hide(id) {
 	var oId = document.getElementById(id);
-	oId.style.display = "none";
+	oId.style.display = 'none';
 }
